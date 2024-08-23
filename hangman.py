698cbedf784da71a7de5791e1ffe6c91c69c2fb6
Nitish Kumar S <62009174+nitishkumar2701@users.noreply.github.com>
2024-08-23 11:24:37 +0530

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,48 @@
+import random
+import hangman_stages
+import wordList
+
+
+randomWordList = []
+blankList = []
+remainingLives = 0
+
+wordList = wordList.words
+randomChoiceWord = random.choice(wordList)
+randomChoiceWord = randomChoiceWord.lower()
+blanksCount = len(randomChoiceWord)
+
+for i in range(blanksCount):
+    blankList.append("_")
+
+for letter in randomChoiceWord:
+    randomWordList.append(letter)
+
+print("Welcome to Hangman game ! \n")
+print("Rules: \n Guess the word hiding behind the blanks ! \n You've got 6 lives \n These words are related to computer science \n Let's Begin \n ")
+print(blankList)
+
+while(remainingLives < 6 and "_" in blankList):
+    userinputLetter = input("\n \nChoose a letter :")
+    if(userinputLetter in randomWordList):
+        print("Keep Going Find the next letter")
+        foundIndex = randomWordList.index(userinputLetter)
+        blankList[foundIndex] = userinputLetter
+        randomWordList[foundIndex] = "-"
+        print(blankList)
+    elif(userinputLetter not in randomWordList):
+        remainingLives = remainingLives + 1
+        print(f"Oops wrong guess, you have used {remainingLives} chance out of 6")
+        print(hangman_stages.hangman_stages_list[remainingLives])
+    else:
+        print("Keep Going Find the next letter")
+        print(blankList)
+
+if(remainingLives == 6):
+    print("\n Oops you failed to save the hangman , Game over :( \n")
+    print(f" Word behind the blanks -> {randomChoiceWord} \n")
+    print(hangman_stages.sad_ascii)
+else:
+    print("\n Great you won the game, you saved the hangman ! \n")
+    print(hangman_stages.happy_ascii)
+
